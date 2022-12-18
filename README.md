bcbb96221b2b26a7eb16c4550846e92e13d1024d
sargent-michael <sargent.michael@protonmail.com>
2022-12-17 20:49:17 -0500

    Commit

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # PyMat.py 
 
-PyMat is a simple CTF project directory automation tool. Often times, I find myself creating a directory, then cd'ing into, and then being able to start my enumeration workflow. This script is to help with that. Right now it is a little bare-bones because it just creates a project directory in the the CTF platform directory that you are using at that time. 
+PyMat is a simple CTF project directory automation tool. Often times, I find myself creating a directory, then cd'ing into, and then being able to start my enumeration workflow. This script is to help with that. Right now it is a little bare-bones because it just creates a project directory in the the CTF platform directory that you are using at that time. You can also list the current project folders in those CTF folders with the -l option.
 
-**PLEASE NOTE:** In order for this script to work for you, you need to create a htb (hack-the-box)and thm (try-hack-me) directory in your documents folder as well as replacing the directory on line 25 to your users documents directory.
+**PLEASE NOTE:** In order for this script to work for you, you need to create a htb (hack-the-box)and thm (try-hack-me) directory in your documents folder as well as replacing the directory on lines 14 and 28 to your users documents directory.
 
 ## Example of Usuage
 Let's take a look at the the help menu.
 ```python
-usage: pymat.py [-h] -c  -p
+./pymat.py -h
+usage: pymat.py [-h] [-c] [-l] [-p]
 
 Simple Python Port Scanner
 
 options:
   -h, --help       show this help message and exit
   -c , --ctf       ctf platform - i.e. "hack the box" or "try hackme"
+  -l , --list      list the contents of a CTF Directory
   -p , --project   the name of target machine
 ```
 
@@ -39,4 +41,21 @@ As you can see here, we inputted an incorrect CTF Platform and got an error
 ./pymat.py -c adf -p Test
 [+] Validating CTF Platform
 [-] Platform "adf" does not exist...
+```
+
+List of htb Projects
+```python
+./pymat.py -l htb          
+[+] Checking for htb project folders
+[+] Printing htb project folders...
+
+shoppy
+support
+```
+
+List error
+```python
+./pymat.py -l adsf
+[+] Checking for adsf project folders
+[-] Platform "adsf" does not exist...
 ```
--- pymat.py
+++ pymat.py
@@ -4,48 +4,71 @@ import os
 from colorama import Fore
 
 
-def main():
-    # Arguments
-    parser = argparse.ArgumentParser(description='Simple Python Port Scanner')
-    parser.add_argument('-c', '--ctf', action="store", dest="ctf", help='ctf platform - i.e. \"hack the box\" or \"try hackme\"', metavar='', required=True)
-    parser.add_argument('-p', '--project', action="store", dest="project", help='the name of target machine', metavar='', required=True)
 
-    # Args to Vars
-    args = parser.parse_args()
-    platform = args.ctf
-    project = args.project
+def list_dir(dir_list):
+    # Checks for valid CTF folder
+    print(f"[+] Checking for {dir_list} project folders")
+    if dir_list == 'htb' or dir_list == 'thm':
+        # List the project folder in that Directory
+        print(f"[+] Printing {dir_list} project folders...\n")
+        ctf_dir = os.listdir(f'/<USER>/<PATH>/Documents/{dir_list}')
+        for i in ctf_dir:
+            print(i)
     
-    print("[+] Validating CTF Platform")
+    # If the user does not input a correct ctf, then it will exit out...
+    else:
+       print(Fore.RED + f"[-] Platform \"{dir_list}\" does not exist...")
 
+def make_dir(platform, project):
+    print("[+] Validating CTF Platform")
     # Checks to see if the user entered a correct CTF Platform
     if platform == 'htb' or platform == 'thm':
-
         # Checks to see if the path already exists
         print("[+] Checking to see if that directory already exists")
         dir_check = os.path.exists(f'/<USER>/<PATH>/Documents/{platform}/{project}')
-        
+
         # If the path does not already exist then it will continue
         if dir_check == False:
-
             # Generates a new CTF Project in the given CTF dir
             if platform == 'htb':
                 print("[+] Creating a new directory for htb")
                 os.system(f"mkdir ~/Documents/htb/{project}")
                 print(Fore.GREEN + "[+] Directory creation was successful")
-
             elif platform == 'thm':
                 print("[+] Creating a new directory for thm")
                 os.system(f"mkdir ~/Documents/thm/{project}")
                 print(Fore.GREEN + "[+] Directory creation was successful")
-
+        
         # If that dir already exists, then it will exit out...
         else:
                 print(Fore.RED + "[-] That project already exists...")
     
     # If the user does not input a correct ctf, then it will exit out...
     else:
-        print(Fore.RED + f"[-] Platform \"{platform}\" does not exist...")
-        
+       print(Fore.RED + f"[-] Platform \"{platform}\" does not exist...")
+
+
+def main():
+    # Arguments
+    parser = argparse.ArgumentParser(description='Simple Python Port Scanner')
+    parser.add_argument('-c', '--ctf', action="store", dest="ctf", help='ctf platform - i.e. \"hack the box\" or \"try hackme\"', metavar='')
+    parser.add_argument('-l', '--list', action="store", dest="list", help='list the contents of a CTF Directory', metavar='')
+    parser.add_argument('-p', '--project', action="store", dest="project", help='the name of target machine', metavar='')
+
+    # Args to Vars
+    args = parser.parse_args()
+    dir_list = args.list
+    platform = args.ctf
+    project = args.project
+
+    # Determines what to do
+    if project is not None:
+        make_dir(platform, project)
+    elif dir_list is not None:
+        list_dir(dir_list)
+    else:
+        parser.print_help()
+       
 
 if __name__ == "__main__":
-    main()    
+    main()  
